0b954a46af3187c466c91f4eb4df0ad3a6fa72bb
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-09-28 18:28:42 +0200

    rename module constants and fix some unit test bugs

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -19,7 +19,7 @@ namespace Modules\MyPrivate\tests\Admin;
  */
 class AdminTest extends \PHPUnit\Framework\TestCase
 {
-    protected const MODULE_NAME = 'MyPrivate';
+    protected const NAME = 'MyPrivate';
 
     protected const URI_LOAD = '';
 
