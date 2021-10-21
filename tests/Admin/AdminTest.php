999d1558a2e08e01a31bd76996a0f419c8662b97
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:19:51 +0200

    make classes final

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -17,7 +17,7 @@ namespace Modules\MyPrivate\tests\Admin;
 /**
  * @internal
  */
-class AdminTest extends \PHPUnit\Framework\TestCase
+final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'MyPrivate';
 
